4130b366314c6469662af54dbf7baa28ac7a9ce8
Aline Brandão <alinep02brandao@gmail.com>
2025-10-29 22:01:50 -0300

    first commit

---
+++ src/main/java/com/alinebrandaoprojects/Projeto/Web/Services/ProjetoWebServicesApplication.java
@@ -0,0 +1,15 @@
+package com.alinebrandaoprojects.Projeto.Web.Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesApplication.class, args);
+
+        // pelo amor, não sei mais o que faço
+	}
+
+}
